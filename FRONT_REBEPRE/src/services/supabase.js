653e439e68d795484765f8bbd839ca5565ca27c8
Enma0101 <enmanuel.aft@gmail.com>
2025-07-21 11:55:01 -0400

    npm

--- FRONT_REBEPRE/src/services/supabase.js
+++ FRONT_REBEPRE/src/services/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://fqfwfwjufggyhqzrnzrs.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
